c5bb736f2ecdfb81e873c5e067f803f3347bdb7f
hieu.tm <hieu.tm@dnpcorp.vn>
2026-05-08 09:07:30 +0700

    [feat]: hrm

---
+++ lesson-02-import-export/main.js
@@ -0,0 +1,3 @@
+import { myVar, myFunction } from "./named.js";
+
+console.log(myVar);
---
+++ lesson-02-import-export/named.js
@@ -0,0 +1,7 @@
+const myVar = 20;
+
+function myFunction() {
+  console.log("My function");
+}
+
+export { myVar, myFunction };
